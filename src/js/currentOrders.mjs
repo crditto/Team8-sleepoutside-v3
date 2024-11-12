c92540d5d33d308e93b89d4fcfe8ee7218704c88
vbisso <valeriabisso19@gmail.com>
2024-11-11 20:33:58 -0700

    team8

---
+++ src/js/currentOrders.mjs
@@ -0,0 +1,22 @@
+import { loadHeaderFooter } from "./utils.mjs";
+import { getOrders } from "./externalServices.mjs";
+import { checkLogin } from "../js/auth.mjs";
+
+export default async function currentOrders(selector, token) {
+  try {
+    const orders = await getOrders(token);
+    const parent = document.querySelector(`${selector} tbody`);
+    parent.innerHTML = orders.map(orderTemplate).join("");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function orderTemplate(order) {
+  return `<tr><td>${order.id}</td>
+  <td>${new Date(order.orderDate).toLocaleDateString("en-US")}</td>
+  <td>${order.items.length}</td>
+  <td>${order.orderTotal}</td></tr>`;
+}
+const token = checkLogin();
+currentOrders("#orders", token);
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         product: resolve(__dirname, "src/product_pages/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product_list: resolve(__dirname, "src/product-list/index.html"),
+        orders: resolve(__dirname, "src/orders/index.html"),
       },
     },
   },
